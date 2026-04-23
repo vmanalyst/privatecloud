5875f41602a8752751a2081a2f8b8cb9bbf493d7
VMAnalyst.com <273811044+vmanalyst@users.noreply.github.com>
2026-04-23 19:07:18 +0100

    Create add-uefi.allowAuthBypass-singlevm.ps1

---
+++ add-uefi.allowAuthBypass-singlevm.ps1
@@ -0,0 +1,10 @@
+$VM = Get-VM -Name "YOUR-VM-NAME"
+
+$spec      = New-Object VMware.Vim.VirtualMachineConfigSpec
+$opt       = New-Object VMware.Vim.OptionValue
+$opt.Key   = "uefi.allowAuthBypass"
+$opt.Value = "TRUE"
+$spec.ExtraConfig = @($opt)
+$VM.ExtensionData.ReconfigVM($spec)
+
+Write-Host "Done - uefi.allowAuthBypass set on $($VM.Name)" -ForegroundColor Green
